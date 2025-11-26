5d5bb4ad630971064f9a244ac2b9923e500a4367
msi-end <aditya01377@gmail.com>
2025-11-26 12:16:27 +0530

    bug fix and updated api

--- backend/controllers/coreEntityControllers/project_phaseController/index.js
+++ backend/controllers/coreEntityControllers/project_phaseController/index.js
@@ -21,6 +21,28 @@ class projectPhaseCoreController {
          return res.status(500).json({ status: false, msg: 'Internal Server Error' });
       }
    }
+   static async getProject_PhaseList(req, res) {
+      try {
+         const { pro_id } = req.params;
+         if (!pro_id) {
+            return res.status(400).json({ status: false, msg: 'pro_id required' });
+         }
+         const rows = await projectPhaseCoreModel.getProject_PhaseList(pro_id);
+         console.log(rows);
+         
+         if (!rows) {
+            return res.status(404).json({ status: false, msg: 'Project phase not found or status unchanged' });
+         }
+         return res.status(200).json({
+            status: true,
+            msg: 'Project phase status updated successfully',
+            data:rows,
+         });
+      } catch (error) {
+         console.error('Error updating project phase status:', error);
+         return res.status(500).json({ status: false, msg: 'Internal Server Error' });
+      }
+   }
 }
 
 module.exports = projectPhaseCoreController;
--- backend/models/coreEntityModels/project_phaseModel/index.js
+++ backend/models/coreEntityModels/project_phaseModel/index.js
@@ -18,7 +18,21 @@ class projectModel {
          connPool.release();
       }
    }
+   static async getProject_PhaseList(pro_id) {
+      const connPool = await pool.getConnection();
+      try {
+         const [result] = await connPool.query('SELECT p.phase_name,pp.* FROM project_phase pp LEFT JOIN phases p ON pp.phase_id=p.phase_id  WHERE pro_id =?;', [
+           pro_id,
+         ]);
+         
+         return result;
+      } catch (error) {
+         console.error('Error updating pro_phase_status:', error);
+         throw error;
+      } finally {
+         connPool.release();
+      }
+   }
 }
-
 
 module.exports = projectModel;
