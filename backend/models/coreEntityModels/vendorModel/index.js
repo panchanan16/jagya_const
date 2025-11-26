7b29f7f0aee5864bbf06fb6d5d0bb6adfc166e0d
msi-end <aditya01377@gmail.com>
2025-11-26 12:38:03 +0530

    new api updates and bug fix

--- backend/models/coreEntityModels/vendorModel/index.js
+++ backend/models/coreEntityModels/vendorModel/index.js
@@ -24,7 +24,7 @@ class VendorModel {
    }
    static async getVendor_Purcheses_Payments(vendor_id) {
       const query = `SELECT * FROM vendor_payments WHERE pay_vendor_id=?;
-                     SELECT v.vendor_name,p.pro_name,c.client_name, mil.vendor_id ,mil.mr_item_id, mil.mr_project_r_id, mil.mr_item_name, mil.mr_item_quantity, mil.mr_item_amount, mil.mr_item_date FROM material_item_list mil LEFT JOIN vendors v ON v.vendor_id=mil.vendor_id LEFT JOIN projects p on p.pro_id= mil.mr_project_r_id LEFT JOIN clients c ON c.client_id=p.pro_client_r_id WHERE vendor_id=? AND fd_approval=1;
+                     SELECT v.vendor_name,p.pro_name,c.client_name, mil.vendor_id ,mil.mr_item_id, mil.mr_project_r_id, mil.mr_item_name, mil.mr_item_quantity, mil.mr_item_amount, mil.mr_item_date FROM material_item_list mil LEFT JOIN vendors v ON v.vendor_id=mil.vendor_id LEFT JOIN projects p on p.pro_id= mil.mr_project_r_id LEFT JOIN clients c ON c.client_id=p.pro_client_r_id WHERE mil.vendor_id=? AND mil.fd_approval=1;
                      SELECT (SELECT SUM(pay_amount) FROM vendor_payments WHERE pay_vendor_id = ?) AS total_payments,(SELECT SUM(mr_item_amount) FROM material_item_list WHERE vendor_id = ? AND fd_approval = 1) AS total_materials;
                      SELECT * FROM vendors WHERE vendor_id=?`;
       const connPool = await pool.getConnection();
--- backend/models/entityModels/collectionModel/index.js
+++ backend/models/entityModels/collectionModel/index.js
@@ -12,38 +12,44 @@ class CollectionsModel {
    }
 
    // Get all collectionss
-   static async findAll( from_date = null, to_date = null) {
-      let query = `
+   static async findAll(from_date, to_date) {
+   let query = `
       SELECT collections.*, projects.pro_name, c.client_name
       FROM collections
       JOIN projects ON collections.col_project_id = projects.pro_ref_no
       LEFT JOIN clients c ON c.client_id = projects.pro_client_r_id
    `;
 
-      const params = [];
-      if (from_date && to_date) {
-         query += ` WHERE DATE(collections.col_date BETWEEN ? AND ? `;
-         params.push(from_date, to_date);
-      } else if (from_date) {
-         query += ` WHERE DATE(collections.col_date) >= ? `;
-         params.push(from_date);
-      } else if (to_date) {
-         query += ` WHERE DATE(collections.col_date) <= ? `;
-         params.push(to_date);
-      }
+   const params = [];
 
-      query += ` ORDER BY collections.col_id DESC;`;
-      const connPool = await pool.getConnection();
-      try {
-         const [rows] = await connPool.query(query, params);
-         return rows;
-      } catch (error) {
-         console.error('Error retrieving all collections:', error);
-         throw error;
-      } finally {
-         connPool.release();
-      }
+   if (from_date && to_date) {
+      query += ` WHERE DATE(collections.col_date) BETWEEN ? AND ? `;
+      params.push(from_date, to_date);
+
+   } else if (from_date) {
+      query += ` WHERE DATE(collections.col_date) >= ? `;
+      params.push(from_date);
+
+   } else if (to_date) {
+      query += ` WHERE DATE(collections.col_date) <= ? `;
+      params.push(to_date);
    }
+
+   query += ` ORDER BY collections.col_id DESC;`;
+
+   const connPool = await pool.getConnection();
+
+   try {
+      const [rows] = await connPool.query(query, params);
+      return rows;
+   } catch (error) {
+      console.error('Error retrieving all collections:', error);
+      throw error;
+   } finally {
+      connPool.release();
+   }
+}
+
 
    // Get a single collections by ID
    static async findOne(col_id) {
